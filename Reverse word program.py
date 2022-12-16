6e81474ca5b1893258790fcc55b45322efb49f8f
kranthijandyala <120300636+kranthijandyala@users.noreply.github.com>
2022-12-16 15:37:04 +0530

    Add files via upload

---
+++ Reverse word program.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Write a Python program that accepts a word from the user and reverse it.
+# In[5]:
+m=input('enter a word')
+n=" "
+for i in m:
+    n=i+n
+print("The original word", m)
+print("The reverse word", n)
+
+
+
+
